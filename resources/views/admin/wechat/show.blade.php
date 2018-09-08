9d26525f4022116ef517786908d3c46f617d45b2
serrt <1163816051@qq.com>
2018-09-08 16:49:52 +0800

    show

---
+++ resources/views/admin/wechat/show.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layouts.iframe')
+@section('content')
+    <div class="box box-info">
+        <div class="box-header with-border">
+            <a href="javascript:history.back()" class="btn btn-default"> 返回</a>
+        </div>
+
+        <div class="box-body">
+        </div>
+    </div>
+@endsection
