5a41ebc29391d396a0885edf44fc32230fff2f1c
panliang <1163816051@qq.com>
2018-09-10 14:58:20 +0800

    app.name

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -18,9 +18,9 @@
         <!-- Logo -->
         <a href="/" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>A</b>LT</span>
+            <span class="logo-mini">{{config('app.name')}}</span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Admin</b>LTE</span>
+            <span class="logo-lg">{{config('app.name')}}</span>
         </a>
         <!-- 顶部菜单 -->
         <nav class="navbar navbar-static-top">
