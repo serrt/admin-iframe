162ec9ec2931f87debac17e1ba72ca93f4d2e4d2
panliang <1163816051@qq.com>
2018-09-12 12:00:23 +0800

    home-link

--- resources/views/admin/index/home.blade.php
+++ resources/views/admin/index/home.blade.php
@@ -2,33 +2,31 @@
 @section('content')
 <div class="row">
     <div class="col-md-3 col-xs-12">
-        <div class="info-box bg-blue">
+        <a href="{{route('admin.wechat.index')}}" class="info-box bg-blue">
             <span class="info-box-icon"><i class="fa fa-apple"></i></span>
             <div class="info-box-content">
                 <span class="info-box-text">APP总数</span>
                 <span class="info-box-number">{{$wechat_count}}</span>
-
-                <span class="progress-description">APP总数</span>
             </div>
-        </div>
+        </a>
     </div>
     <div class="col-md-3 col-xs-12">
-        <div class="info-box bg-yellow">
+        <a href="{{route('admin.wechat_users.index')}}" class="info-box bg-yellow">
             <span class="info-box-icon"><i class="fa fa-users"></i></span>
             <div class="info-box-content">
                 <span class="info-box-text">用户总数</span>
                 <span class="info-box-number">{{$user_count}}</span>
             </div>
-        </div>
+        </a>
     </div>
     <div class="col-md-3 col-xs-12">
-        <div class="info-box bg-green">
+        <a href="{{route('admin.wechat_user_msg.index')}}" class="info-box bg-green">
             <span class="info-box-icon"><i class="fa fa-comment"></i></span>
             <div class="info-box-content">
                 <span class="info-box-text">资料总数</span>
                 <span class="info-box-number">{{$message_count}}</span>
             </div>
-        </div>
+        </a>
     </div>
 </div>
 @endsection
