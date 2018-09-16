3fb37c49b2a19ff8f5bdb878edf4ab0b3203f9b2
serrt <1163816051@qq.com>
2018-09-16 11:53:36 +0800

    add

---
+++ database/migrations/2018_09_16_114236_create_regions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateRegionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('regions', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('pid')->unsigned()->default(0);
+			$table->string('name', 100);
+			$table->integer('level')->default(1);
+			$table->string('code')->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('regions');
+	}
+
+}
