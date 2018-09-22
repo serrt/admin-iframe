597a94eb0086bd975b77943c80a3a0927a49d8db
pl <pl@pldeMacBook-Air.local>
2018-09-22 19:07:34 +0800

    blade @can

--- resources/views/admin/menu/index.blade.php
+++ resources/views/admin/menu/index.blade.php
@@ -2,7 +2,9 @@
 @section('content')
     <div class="box box-info">
         <div class="box-header with-border">
+            @can('admin.menu.create')
             <a href="{{route('admin.menu.create')}}" class="btn btn-default"><i class="fa fa-plus"></i> 添加</a>
+            @endcan
         </div>
         <div class="box-body row">
             <div class="col-md-6" onselectstart="return false;">
