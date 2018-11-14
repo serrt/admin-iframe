42a204c1ff83b4a236dbb42821d87865dabce16d
serrt <1163816051@qq.com>
2018-11-14 18:56:56 +0800

    base64

--- app/Http/Controllers/Api/WebController.php
+++ app/Http/Controllers/Api/WebController.php
@@ -17,51 +17,52 @@ class WebController extends Controller
 {
     public function upload(Request $request)
     {
-        $files = $request->file();
         $path = $request->input('path', 'uploads/'.date('Y-m-d'));
         $result = [];
+
+        // file 文件
+        $files = $request->file();
         foreach ($files as $key => $fileData) {
             $item = null;
             if (is_array($fileData)) {
                 foreach ($fileData as $file) {
-                    logger($file->getSize());
-                    $savePath = $file->store($path);
-                    $item[] = Storage::url($savePath);
+                    $item[] = Storage::url($this->saveFile($path, $file));
                 }
             } else {
-                $savePath = $fileData->store($path);
-                $item = Storage::url($savePath);
+                $item = Storage::url($this->saveFile($path, $fileData));
             }
             $result[$key] = $item;
         }
 
         // base64 图片
-        foreach ($request->post() as $key => $files) {
+        $data = $request->post();
+        foreach ($data as $key => $files) {
             $item = null;
             if (is_array($files)) {
                 foreach ($files as $file) {
-                    if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $file, $matches)) {
-                        $type = $matches[2];
-                        if(in_array($type,array('jpeg','jpg','gif','bmp','png'))) {
-                            $savePath = $path . '/' . uniqid() . '.' . $type;
-                            Storage::put($savePath, base64_decode(str_replace($matches[1], '', $file)));
-                            $item[] = Storage::url($savePath);
-                        }
-                    }
+                    $item[] = Storage::url($this->saveFile($path, $file));
                 }
             } else {
-                if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $files, $matches)) {
-                    $type = $matches[2];
-                    if(in_array($type,array('jpeg','jpg','gif','bmp','png'))) {
-                        $savePath = $path . '/' . uniqid() . '.' . $type;
-                        Storage::put($savePath, base64_decode(str_replace($matches[1], '', $files)));
-                        $item = Storage::url($savePath);
-                    }
-                }
+                $item = Storage::url($this->saveFile($path, $files));
             }
             $result[$key] = $item;
         }
         return $this->json($result);
+    }
+
+    protected function saveFile($path, $file = null)
+    {
+        if (gettype($file) == 'object') {
+            $file = Storage::putFile($path, $file);
+        } else if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $file, $result)) {
+            $type = $result[2];
+            if(in_array($type,array('jpeg','jpg','gif','bmp','png'))) {
+                $savePath = $path . '/' . uniqid() . '.' . $type;
+                Storage::put($savePath, base64_decode(str_replace($result[1], '', $file)));
+                $file = $savePath;
+            }
+        }
+        return $file;
     }
 
     public function permission(Request $request)
