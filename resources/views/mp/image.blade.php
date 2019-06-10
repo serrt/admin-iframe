d76625eea4b7fc560a8a28506a7378746dd1028c
serrt <1163816051@qq.com>
2019-06-10 11:06:28 +0800

    test mp

--- resources/views/mp/image.blade.php
+++ resources/views/mp/image.blade.php
@@ -15,9 +15,13 @@
                                 <div class="weui-uploader__info"><span id="uploadCount">0</span>/5</div>
                             </div>
                             <div class="weui-uploader__bd">
-                                <ul class="weui-uploader__files" id="uploaderFiles"></ul>
+                                <ul class="weui-uploader__files" id="uploaderFiles">
+                                    <li class="weui-uploader__file">
+                                        <img src="{{asset('images/403.jpeg')}}" onclick="gallery(this)" alt="" width="100%" height="100%">
+                                    </li>
+                                </ul>
                                 <div class="weui-uploader__input-box">
-                                    <input id="uploaderInput" class="weui-uploader__input" type="file" accept="image/*" capture="camera" multiple="" />
+                                    <input id="uploaderInput" class="weui-uploader__input" type="text" accept="image/*" capture="camera" multiple="" />
                                 </div>
                             </div>
                         </div>
@@ -32,12 +36,20 @@
 <script src="https://cdn.bootcss.com/vConsole/3.3.0/vconsole.min.js"></script>
 <script type="text/javascript" charset="utf-8">
     var vConsole = new VConsole();
+    function gallery (ele) {
+        var bt = weui.gallery(ele.src, {
+            onDelete: function(){
+                // if(confirm('确定删除该图片？')){ console.log('删除'); }
+                bt.hide();
+            }
+        })
+    }
     wx.config(<?php echo $app->jssdk->buildConfig(array('chooseImage', 'previewImage', 'uploadImage', 'downloadImage'), false) ?>);
     wx.ready(function () {
         wx.chooseImage({
             count: 1, // 默认9
             sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
             success: function (res) {
                 var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                 console.log(localIds)
