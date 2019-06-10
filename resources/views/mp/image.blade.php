8b6e466fe21cd0e41a02fb783353311ed9a0738f
serrt <1163816051@qq.com>
2019-06-10 11:39:00 +0800

    test mp

--- resources/views/mp/image.blade.php
+++ resources/views/mp/image.blade.php
@@ -11,7 +11,7 @@
                     <div class="weui-cell__bd">
                         <div class="weui-uploader">
                             <div class="weui-uploader__hd">
-                                <p class="weui-uploader__title">图片上传</p>
+                                <p class="weui-uploader__title">H5</p>
                                 <div class="weui-uploader__info"><span id="uploadCount">0</span>/5</div>
                             </div>
                             <div class="weui-uploader__bd">
@@ -21,7 +21,27 @@
                                     </li>
                                 </ul>
                                 <div class="weui-uploader__input-box">
-                                    <input id="uploaderInput" class="weui-uploader__input" type="file" accept="image/*" capture="camera" multiple/>
+                                    <input id="uploader" class="weui-uploader__input" type="file" accept="image/*" capture="camera" multiple/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="weui-cell">
+                    <div class="weui-cell__bd">
+                        <div class="weui-uploader">
+                            <div class="weui-uploader__hd">
+                                <p class="weui-uploader__title">WS</p>
+                                <div class="weui-uploader__info"><span id="uploadCount">0</span>/5</div>
+                            </div>
+                            <div class="weui-uploader__bd">
+                                <ul class="weui-uploader__files" id="uploaderFiles">
+                                    <li class="weui-uploader__file">
+                                        <img src="{{asset('images/403.jpeg')}}" onclick="gallery(this)" alt="" width="100%" height="100%">
+                                    </li>
+                                </ul>
+                                <div class="weui-uploader__input-box">
+                                    <input class="weui-uploader__input" type="text" onclick="chooseImage()"/>
                                 </div>
                             </div>
                         </div>
@@ -56,5 +76,71 @@
         });
     }
     wx.config(<?php echo $app->jssdk->buildConfig(array('chooseImage', 'previewImage', 'uploadImage', 'downloadImage'), false) ?>);
+
+    var uploadCount = 0;
+    weui.uploader('#uploader', {
+        url: '{{route('api.web.upload')}}',
+        auto: true,
+        type: 'file',
+        fileVal: 'fileVal',
+        compress: {
+            width: 1600,
+            height: 1600,
+            quality: .8
+        },
+        onBeforeQueued: function(files) {
+            // `this` 是轮询到的文件, `files` 是所有文件
+
+            if(["image/jpg", "image/jpeg", "image/png", "image/gif"].indexOf(this.type) < 0){
+                weui.alert('请上传图片');
+                return false; // 阻止文件添加
+            }
+            if(this.size > 10 * 1024 * 1024){
+                weui.alert('请上传不超过10M的图片');
+                return false;
+            }
+            if (files.length > 5) { // 防止一下子选择过多文件
+                weui.alert('最多只能上传5张图片，请重新选择');
+                return false;
+            }
+            if (uploadCount + 1 > 5) {
+                weui.alert('最多只能上传5张图片');
+                return false;
+            }
+
+            ++uploadCount;
+
+            // return true; // 阻止默认行为，不插入预览图的框架
+        },
+        onQueued: function(){
+
+            // console.log(this.status); // 文件的状态：'ready', 'progress', 'success', 'fail'
+            // console.log(this.base64); // 如果是base64上传，file.base64可以获得文件的base64
+
+            // this.upload(); // 如果是手动上传，这里可以通过调用upload来实现；也可以用它来实现重传。
+            // this.stop(); // 中断上传
+
+            // return true; // 阻止默认行为，不显示预览图的图像
+        },
+        onBeforeSend: function(data, headers){
+            console.log(data);
+            // $.extend(data, { test: 1 }); // 可以扩展此对象来控制上传参数
+            // $.extend(headers, { Origin: 'http://127.0.0.1' }); // 可以扩展此对象来控制上传头部
+
+            return false; // 阻止文件上传
+        },
+        onProgress: function(procent){
+            console.log(this, procent);
+            // return true; // 阻止默认行为，不使用默认的进度显示
+        },
+        onSuccess: function (ret) {
+            console.log(this, ret);
+            // return true; // 阻止默认行为，不使用默认的成功态
+        },
+        onError: function(err){
+            console.log(this, err);
+            // return true; // 阻止默认行为，不使用默认的失败态
+        }
+    });
 </script>
 @endsection
